aae960cb3f84a828fa3fc3a842abe0af3c3cf796
Sandesh Thorat <sandeshthorat13@gmail.com>
2024-07-11 23:26:35 +0530

    main branch changes

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware } from '@clerk/nextjs';
 
-export default authMiddleware({
-    publicRoutes: ["/api/:path*"],
-});
+export default withClerkMiddleware()
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
